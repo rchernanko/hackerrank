f917adb45792a1a84eaaf890476462a269e2b2e8
Richard Chernanko <rchernanko@hotmail.co.uk>
2015-06-05 21:40:19 +0100

    finished the love letter mystery challenge

--- src/algorithm/warmups/LoveLetterMystery.java
+++ src/algorithm/warmups/LoveLetterMystery.java
@@ -6,57 +6,27 @@ public class LoveLetterMystery {
 
     public static void main(String[] args) {
 
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Please enter the number of test cases");
-//        int numberOfTestCases = in.nextInt();
-//        for (int i = 0; i < numberOfTestCases; i++) {
-//            System.out.println("Please enter the number of growth cycles");
-//            int numberOfGrowthCycles = in.nextInt();
-//        }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the number of test cases");
+        int numberOfTestCases = in.nextInt();
 
-        String richard = "abcd";
-//        StringBuilder richard = new StringBuilder("abcd");
-
-        String firstHalf = richard.substring(0, ((richard.length()-1)/2)+1);
-
-        String reversed = new StringBuilder(richard).reverse().toString();
-
-        String secondHalf = reversed.substring(0, ((richard.length()-1)/2)+1);
-
-//        System.out.println(firstHalf);
-//        System.out.println(secondHalf);
-
-        int numberOfOperationsRequired = 0;
-
-        for(int i = 0; i < firstHalf.length(); i++){
-
-            char firstLetter = firstHalf.charAt(i);
-            char lastLetter = secondHalf.charAt(i);
-
-            if(firstLetter > lastLetter){
-                numberOfOperationsRequired = numberOfOperationsRequired + (firstLetter - lastLetter);
+        for (int i = 0; i < numberOfTestCases; i++) {
+            System.out.println("Please enter the original string value");
+            String richard = in.next();
+            String firstHalf = richard.substring(0, ((richard.length() - 1) / 2) + 1);
+            String reversed = new StringBuilder(richard).reverse().toString();
+            String secondHalf = reversed.substring(0, ((richard.length() - 1) / 2) + 1);
+            int numberOfOperationsRequired = 0;
+            for (int j = 0; j < firstHalf.length(); j++) {
+                char firstLetter = firstHalf.charAt(j);
+                char lastLetter = secondHalf.charAt(j);
+                if (firstLetter > lastLetter) {
+                    numberOfOperationsRequired = numberOfOperationsRequired + (firstLetter - lastLetter);
+                } else if (firstLetter < lastLetter) {
+                    numberOfOperationsRequired = numberOfOperationsRequired + (lastLetter - firstLetter);
+                }
             }
-            firstLetter - lastLetter = 0
-
+            System.out.println(numberOfOperationsRequired);
         }
-
-
-        firstHalf.length();
-
-
-
-
-//        System.out.println(richard.length());
-//
-//
-//
-//
-//        int firstLetter = richard.charAt(0);
-//
-//        System.out.println(richard.charAt(richard.length()-1));
-
-
-
-
     }
 }
